0f3276edd4d373843ad753efc639533fcc6d9489
Kyle Rudy <kdrudy@kyles-mbp.apollosrc.root>
2016-12-06 09:43:06 -0600

    Initial commit

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,49 @@
+package com.theironyard;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
+public class Main {
+    public static User user;
+
+    public static void main(String[] args) {
+        Spark.init();
+
+        Spark.get(
+                "/",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    if(user == null) {
+                        return new ModelAndView(m, "login.html");
+                    } else {
+                        m.put("name", user.name);
+                        return new ModelAndView(m, "home.html");
+                    }
+                }),
+                new MustacheTemplateEngine()
+        );
+
+        Spark.post(
+                "/login",
+                ((request, response) -> {
+                    String name = request.queryParams("loginName");
+                    user = new User(name);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    user = null;
+                    response.redirect("/");
+                    return "";
+                })
+        );
+    }
+}
+
---
+++ src/com/theironyard/User.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+/**
+ * Created by kdrudy on 12/5/16.
+ */
+public class User {
+    String name;
+
+    public User(String n) {
+        this.name = n;
+    }
+}
